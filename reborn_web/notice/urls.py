8aa20ff0cb1e28a264a94c0e63b8759046288270
ParkHyeonChae <park19996@naver.com>
2020-01-24 08:11:50 +0900

    notice template 수정, DetailView 구현

--- reborn_web/notice/urls.py
+++ reborn_web/notice/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'notice'
 
 urlpatterns = [
-    path('', views.NoticeList.as_view(), name='notice'),
+    path('', views.NoticeListView.as_view(), name='notice'),
+    path('<int:pk>/', views.NoticeDetailView.as_view(), name='notice_detail'),
 ]
--- reborn_web/notice/views.py
+++ reborn_web/notice/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Notice
 
-class NoticeList(ListView):
+
+class NoticeListView(ListView):
     model = Notice
     paginate_by = 10
     # template_name = 'notice/notice_list.html'  DEFAULT : <app_label>/<model_name>_list.html
@@ -10,4 +11,21 @@ class NoticeList(ListView):
 
     def get_queryset(self):
         return Notice.objects.order_by('-id')
+        
+        # search_word = self.request.GET.get('search_word', '')
+        # subject_type = self.request.GET.get('subject_type', '')
+        # if search_word : # 검색 된 단어 있으면
+        #     return Board.objects.filter(title__icontains=search_word) or Board.objects.filter(content__icontains=search_word)
+        # if subject_type : # 클릭 된 키워드 있으면
+        #     return Board.objects.filter(subject_type__icontains=subject_type)
+        # return Board.objects.order_by('-id')
 
+
+class NoticeDetailView(DetailView):
+    model = Notice
+
+    def get_object(self):
+        notice = super().get_object()
+        notice.hits += 1
+        notice.save()
+        return notice
