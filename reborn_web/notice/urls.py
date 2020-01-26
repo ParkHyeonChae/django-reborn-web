dc09d86ebec9ce99472146749cf6d5cec181dff7
ParkHyeonChae <park19996@naver.com>
2020-01-27 03:53:25 +0900

    글 수정 구현, 본인 게시글 검사 구현

--- reborn_web/notice/urls.py
+++ reborn_web/notice/urls.py
@@ -5,6 +5,7 @@ app_name = 'notice'
 
 urlpatterns = [
     path('', views.NoticeListView.as_view(), name='notice_list'),
-    path('<int:pk>/', views.notice_detail_view, name='notice_detail'),
     path('write/', views.notice_write_view, name='notice_write'),
+    path('<int:pk>/', views.notice_detail_view, name='notice_detail'),
+    path('<int:pk>/edit/', views.notice_edit_view, name='notice_edit'),
 ]
--- reborn_web/notice/views.py
+++ reborn_web/notice/views.py
@@ -9,59 +9,7 @@ from django.contrib import messages
 from django.urls import reverse
 from .forms import NoticeWriteForm
 from users.models import User
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# class NoticeListView(ListView):
-#     model = Notice
-#     paginate_by = 10
-#     template_name = 'notice/notice_list.html'  #DEFAULT : <app_label>/<model_name>_list.html
-#     context_object_name = 'notice_list'        #DEFAULT : <app_label>_list
-
-#     def get_queryset(self):
-#         search_keyword = self.request.GET.get('q', '')
-#         search_type = self.request.GET.get('type', '')
-#         notice_list = Notice.objects.order_by('-id')
-
-#         if search_keyword :
-#             if search_type == 'all':
-#                 notice_list = notice_list.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword) | Q (writer__user_id__icontains=search_keyword))
-#             elif search_type == 'title_content':
-#                 notice_list = notice_list.filter(Q (title__icontains=search_keyword) | Q (content__icontains=search_keyword))
-#             elif search_type == 'title':
-#                 notice_list = notice_list.filter(title__icontains=search_keyword)    
-#             elif search_type == 'content':
-#                 notice_list = notice_list.filter(content__icontains=search_keyword)    
-#             elif search_type == 'writer':
-#                 notice_list = notice_list.filter(writer__user_id__icontains=search_keyword)
-#         if notice_list :
-#             return notice_list
-#         else:
-#             messages.error(self.request, '일치하는 검색 결과가 없습니다.')
-#             notice_list = Notice.objects.order_by('-id')
-#             return notice_list
-#         # return notice_list
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         paginator = context['paginator']
-#         page_numbers_range = 5
-#         max_index = len(paginator.page_range)
-
-#         page = self.request.GET.get('page')
-#         current_page = int(page) if page else 1
-
-#         start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
-#         end_index = start_index + page_numbers_range
-#         if end_index >= max_index:
-#             end_index = max_index
-
-#         page_range = paginator.page_range[start_index:end_index]
-#         context['page_range'] = page_range
-
-#         search_keyword = self.request.GET.get('q', '')
-#         context['q'] = search_keyword
-
-#         return context
 
 class NoticeListView(ListView):
     model = Notice
@@ -123,8 +71,15 @@ def notice_detail_view(request, pk):
     # notice = Notice.objects.filter(id=pk)
     session_cookie = request.session['user_id']
     cookie_name = F'notice_hits:{session_cookie}'
+
+    if request.user == notice.writer:
+        notice_auth = True
+    else:
+        notice_auth = False
+
     context = {
         'notice': notice,
+        'notice_auth': notice_auth,
     }
     response = render(request, 'notice/notice_detail.html', context)
 
@@ -145,6 +100,7 @@ def notice_detail_view(request, pk):
         return response
     return render(request, 'notice/notice_detail.html', context)
 
+
 @login_message_required
 def notice_write_view(request):
     if request.method == "POST":
@@ -164,3 +120,26 @@ def notice_write_view(request):
     return render(request, "notice/notice_write.html", {'form': form})
 
 
+@login_message_required
+def notice_edit_view(request, pk):
+    notice = Notice.objects.get(id=pk)
+    user = request.session['user_id']
+
+    if request.method == "POST" and notice.writer == request.user:
+        print('post')
+        form = NoticeWriteForm(request.POST, instance=notice)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "수정되었습니다.")
+            return redirect('/notice/'+str(pk))
+    else:
+        print('get')
+        user = request.session['user_id']
+        notice = Notice.objects.get(id=pk)
+    
+        if notice.writer == request.user:
+            form = NoticeWriteForm(instance=notice)
+            return render(request, "notice/notice_write.html", {'form': form})
+        else:
+            messages.error(request, "본인 게시글이 아닙니다.")
+            return redirect('/notice/'+str(pk))
