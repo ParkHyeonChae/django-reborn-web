b86547ffc8a7e96500666eb147db5194213c860b
ParkHyeonChae <park19996@naver.com>
2020-02-27 21:12:37 +0900

    시험시간표 구현, 템플릿 일부 적용, 글쓰기 폼 upload icon 추가

--- reborn_web/timetable/urls.py
+++ reborn_web/timetable/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'timetable'
 
 urlpatterns = [
-    path('', views.time_table_view, name='timetable'),
+    path('', views.time_table_view, name='timetable_list'),
 ]
--- reborn_web/timetable/views.py
+++ reborn_web/timetable/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import TimeTable
 
 def time_table_view(request):
-    pass
+    first_timetable_list = TimeTable.objects.filter(grade='first').order_by('-created')
+    second_timetable_list = TimeTable.objects.filter(grade='second').order_by('-created')
+    third_timetable_list = TimeTable.objects.filter(grade='third').order_by('-created')
+    fourth_timetable_list = TimeTable.objects.filter(grade='fourth').order_by('-created')
+    
+    context = {
+        'first_timetable_list': first_timetable_list,
+        'second_timetable_list': second_timetable_list,
+        'third_timetable_list': third_timetable_list,
+        'fourth_timetable_list': fourth_timetable_list,
+    }
+    return render(request, 'timetable/timetable_list.html', context)
