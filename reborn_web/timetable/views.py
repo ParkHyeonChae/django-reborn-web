d3b00689e5b772d45c1770f223a292e4c48b8ae8
ParkHyeonChae <park19996@naver.com>
2020-02-28 02:21:17 +0900

    학년별 시험시간표 출력, nav tab 적용

--- reborn_web/timetable/views.py
+++ reborn_web/timetable/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from .models import TimeTable
 
 def time_table_view(request):
-    first_timetable_list = TimeTable.objects.filter(grade='first').order_by('-created')
-    second_timetable_list = TimeTable.objects.filter(grade='second').order_by('-created')
-    third_timetable_list = TimeTable.objects.filter(grade='third').order_by('-created')
-    fourth_timetable_list = TimeTable.objects.filter(grade='fourth').order_by('-created')
-    
+    # first_timetable_list = TimeTable.objects.filter(grade='first').order_by('-created')
+    # second_timetable_list = TimeTable.objects.filter(grade='second').order_by('-created')
+    # third_timetable_list = TimeTable.objects.filter(grade='third').order_by('-created')
+    # fourth_timetable_list = TimeTable.objects.filter(grade='fourth').order_by('-created')
+
+    timetable_list = TimeTable.objects.all()
+
     context = {
-        'first_timetable_list': first_timetable_list,
-        'second_timetable_list': second_timetable_list,
-        'third_timetable_list': third_timetable_list,
-        'fourth_timetable_list': fourth_timetable_list,
+        # 'first_timetable_list': first_timetable_list,
+        # 'second_timetable_list': second_timetable_list,
+        # 'third_timetable_list': third_timetable_list,
+        # 'fourth_timetable_list': fourth_timetable_list,
+        'timetable_list': timetable_list,
     }
     return render(request, 'timetable/timetable_list.html', context)
