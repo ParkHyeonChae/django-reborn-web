81146dc405d87f2c965f2102062b0a7a908b4c73
ParkHyeonChae <park19996@naver.com>
2020-08-22 12:28:12 +0900

    heroku 배포 setting

--- reborn_web/reborn_web/settings.py
+++ reborn_web/reborn_web/settings.py
@@ -38,7 +38,8 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
@@ -70,6 +71,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -259,3 +261,10 @@ LOGGING = {
         },
     },
 }
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
